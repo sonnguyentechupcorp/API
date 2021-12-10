b00395ab7acdde87acf5816849a828822f8e8c1f
webdata <you@example.com>
2021-12-10 22:52:43 +0700

    Update command test

--- tests/Console/CommandTest.php
+++ tests/Console/CommandTest.php
@@ -13,8 +13,8 @@ class CommandTest extends TestCase
     public function createUser()
     {
         return User::create([
-            'name' => 'son',
-            'email' => 'son2',
+            'name' => $this->faker->name(),
+            'email' => $this->faker->email(),
             'password' => Hash::make('123456'),
             'birth_date' => Carbon::parse('1998-10-10'),
             'gender' => 1,
@@ -75,12 +75,67 @@ class CommandTest extends TestCase
 
         $this->artisan('demo:user-create')
             ->expectsQuestion('What is your name?', 'son2')
-            ->expectsQuestion('What is your email?', 'son2')
+            ->expectsQuestion('What is your email?', $user->email)
             ->expectsQuestion('What is your password?', '123')
             ->expectsQuestion('What is your birth date?', '')
             ->expectsQuestion('What is your gender?', '')
-            ->expectsQuestion('What is your role?', '')
+            ->expectsQuestion('What is your role?', 'User')
             ->expectsOutput('The email was be taken!')
             ->assertExitCode(0);
+
+        $this->artisan('demo:user-create')
+            ->expectsQuestion('What is your name?', 'son2')
+            ->expectsQuestion('What is your email?', 'sadasd')
+            ->expectsQuestion('What is your password?', '123')
+            ->expectsQuestion('What is your birth date?', 'asdasd')
+            ->expectsQuestion('What is your gender?', '')
+            ->expectsQuestion('What is your role?', 'User')
+            ->assertExitCode(0);
+
+        $this->artisan('demo:user-create')
+            ->expectsQuestion('What is your name?', 'son2')
+            ->expectsQuestion('What is your email?', 'sadasd')
+            ->expectsQuestion('What is your password?', '123')
+            ->expectsQuestion('What is your birth date?', '12-12-2012')
+            ->expectsQuestion('What is your gender?', '')
+            ->expectsQuestion('What is your role?', 'User')
+            ->doesntExpectOutput('The name is required!')
+            ->doesntExpectOutput('The email is required!')
+            ->doesntExpectOutput('The email was be taken!')
+            ->doesntExpectOutput('The password is required!')
+            ->doesntExpectOutput('Created a new user successfully!')
+            ->assertExitCode(0);
+
+        $this->artisan('demo:user-create')
+            ->expectsQuestion('What is your name?', 'son2')
+            ->expectsQuestion('What is your email?', 'sadasd')
+            ->expectsQuestion('What is your password?', '123')
+            ->expectsQuestion('What is your birth date?', 'asd')
+            ->expectsQuestion('What is your gender?', '0')
+            ->expectsQuestion('What is your role?', 'User')
+            ->doesntExpectOutput('The name is required!')
+            ->doesntExpectOutput('The email is required!')
+            ->doesntExpectOutput('The email was be taken!')
+            ->doesntExpectOutput('The password is required!')
+            ->doesntExpectOutput('Created a new user successfully!')
+            ->assertExitCode(0);
+    }
+
+    public function test_import_admin_command_success()
+    {
+
+        $this->artisan('demo:user-create')
+            ->expectsQuestion('What is your name?', 'son')
+            ->expectsQuestion('What is your email?', 'son1234')
+            ->expectsQuestion('What is your password?', '123456')
+            ->expectsQuestion('What is your birth date?', '2013-12-12')
+            ->expectsQuestion('What is your gender?', '0')
+            ->expectsQuestion('What is your role?', ["User"])
+            ->expectsOutput('Created a new user successfully!')
+            ->assertExitCode(0);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'son1234',
+        ]);
     }
 }
